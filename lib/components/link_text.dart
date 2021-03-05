852b9652e132a4e89320b6a6ebbb66ee56ed584c
Wei Chun <tanweichun@outlook.com>
2021-03-05 21:08:41 +0800

    added functionality back to Welcome Page

---
+++ lib/components/link_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class LinkText extends StatelessWidget {
+  const LinkText({
+    Key key,
+    this.text,
+    this.onTap,
+    this.fontSize,
+  }) : super(key: key);
+
+  final String text;
+  final Function onTap;
+  final double fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.bold,
+            color: kPrimaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
