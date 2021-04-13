7d8c15633f9b175190cff5e65a06aeb0c5310cfc
Wei Chun <tanweichun@outlook.com>
2021-04-14 00:51:02 +0800

    renamed folder

---
+++ lib/auth/forgot_password/forgot_password_bloc.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../auth_repository.dart';
+import '../form_submission_status.dart';
+import '../../auth/auth_navigation/auth_cubit.dart';
+import 'forgot_password_event.dart';
+import 'forgot_password_state.dart';
+
+class ForgotPasswordBloc
+    extends Bloc<ForgotPasswordEvent, ForgotPasswordState> {
+  final AuthRepository authRepo;
+  final AuthCubit authCubit;
+
+  ForgotPasswordBloc({this.authCubit, this.authRepo})
+      : super(ForgotPasswordState());
+
+  @override
+  Stream<ForgotPasswordState> mapEventToState(
+      ForgotPasswordEvent event) async* {
+    if (event is ForgotPasswordEmailChanged) {
+      yield state.copyWith(email: event.email);
+    }
+
+    if (event is ForgotPasswordSubmitted) {
+      yield state.copyWith(formSubmissionStatus: SubmissionInProgress());
+
+      try {
+        await authRepo.sendForgotPasswordOtp(
+          email: state.email,
+        );
+        authCubit.showResetPassword(email: state.email);
+        yield state.copyWith(formSubmissionStatus: SubmissionSuccess());
+      } catch (e) {
+        yield state.copyWith(formSubmissionStatus: SubmissionFailure(e));
+      }
+    }
+  }
+}
