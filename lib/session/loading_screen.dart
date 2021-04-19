a5868df55005c46a2520114f9db10db469241752
Wei Chun <tanweichun@outlook.com>
2021-04-20 02:00:41 +0800

    changed loading screen

--- lib/session/loading_screen.dart
+++ lib/session/loading_screen.dart
@@ -1,11 +1,22 @@
+import 'package:compound/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [kPrimaryColor, kSecondaryColor],
+          ),
+        ),
+        child: Center(
+          child: SvgPicture.asset('assets/images/compound_logo.svg'),
+        ),
       ),
     );
   }
