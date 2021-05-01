dc2b84616df8d9166de108548e2320a63b4045d2
Wei Chun <tanweichun@outlook.com>
2021-05-01 22:48:24 +0800

    added cash balance

--- lib/authenticated_session/dashboard/view/dashboard_screen.dart
+++ lib/authenticated_session/dashboard/view/dashboard_screen.dart
@@ -1,3 +1,5 @@
+import 'package:badges/badges.dart';
+import 'package:easy_rich_text/easy_rich_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,6 +11,7 @@ import '../../../components/bottom_nav_bar.dart';
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int counter = 0;
     // Initialization for Sizing Configs
     SizeConfig().init(context);
     return Scaffold(
@@ -22,12 +25,77 @@ class DashboardScreen extends StatelessWidget {
           ),
           painter: BackgroundPainter(),
           child: SafeArea(
-            minimum: EdgeInsets.only(left: 35, right: 35),
+            minimum: EdgeInsets.symmetric(horizontal: 20),
             child: SingleChildScrollView(
               child: Container(
                 height: SizeConfig.screenHeight,
                 child: Column(
                   children: [
+                    SizedBox(height: getProportionateScreenHeight(20)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            EasyRichText(
+                              '\$ 2,705',
+                              defaultStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: getProportionateScreenHeight(25),
+                                fontWeight: FontWeight.bold,
+                              ),
+                              patternList: [
+                                EasyRichTextPattern(
+                                  targetString: '2,705',
+                                  style: TextStyle(
+                                    fontSize: getProportionateScreenHeight(40),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              'Available Cash Balance',
+                              style: TextStyle(
+                                fontSize: getProportionateScreenHeight(14),
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: getProportionateScreenWidth(10),
+                          ),
+                          child: counter != 0
+                              ? Badge(
+                                  toAnimate: false,
+                                  padding:
+                                      EdgeInsets.all(counter < 10 ? 7.5 : 5),
+                                  position: BadgePosition.topEnd(end: -5),
+                                  badgeContent: Text(
+                                    counter.toString(),
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  child: Icon(
+                                    Icons.notifications,
+                                    color: Colors.white,
+                                    size: getProportionateScreenHeight(30),
+                                  ),
+                                )
+                              : Icon(
+                                  Icons.notifications,
+                                  color: Colors.white,
+                                  size: getProportionateScreenHeight(30),
+                                ),
+                        ),
+                      ],
+                    ),
                     Spacer(),
                     Text(
                       'Welcome View',
@@ -57,29 +125,6 @@ class DashboardScreen extends StatelessWidget {
       ),
     );
   }
-}
-
-Widget bottomNavigationBar() {
-  return Container(
-    decoration: BoxDecoration(
-      color: Colors.amber,
-      borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(25), topRight: Radius.circular(25)),
-    ),
-    child: BottomNavigationBar(
-      backgroundColor: Colors.transparent,
-      showUnselectedLabels: true,
-      type: BottomNavigationBarType.fixed,
-      elevation: 0,
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.local_activity), label: 'Activity'),
-        BottomNavigationBarItem(icon: Icon(Icons.inbox), label: 'Inbox'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-      ],
-    ),
-  );
 }
 
 class BackgroundPainter extends CustomPainter {
